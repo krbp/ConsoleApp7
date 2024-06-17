ec4e344736c72c4913dc3e8917d1166aff2946c4
krbp <165799825+krbp@users.noreply.github.com>
2024-06-17 08:58:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance = Singleton.GetInstance();
+            A a = new(instance);
+            B b = new(instance);
+        }
+    }
+    public class A
+    {
+        private readonly Singleton singleton;
+        public A(Singleton instance)
+        {
+            this.singleton = instance;
+        }
+
+    }
+    public class B
+    {
+        private readonly Singleton singleton;
+        public B(Singleton instance)
+        {
+            this.singleton = instance;
+        }
+    }
+
+    public class Singleton
+    {
+        private static Singleton singleton;
+        private Singleton() { }
+        public static Singleton GetInstance()
+        {
+            singleton ??= new Singleton();
+            return singleton;
+        }
+
+    }
+}
